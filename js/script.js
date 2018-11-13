f416fff573d7fbb42011b5a323e48f6cda59876e
nokia6680 <artur.ballod@gmail.com>
2018-11-14 01:52:25 +0200

    #2

--- js/script.js
+++ js/script.js
@@ -17,61 +17,33 @@ function Selected(a) {
   }
 }
 
+$(document).ready(function(){
+  $('.content-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false
+  });
+});
 
-var modalSearch = document.querySelector(".slide-panel");
-var closeButton = document.querySelector(".slide-panel__close");
-var openButton = document.querySelectorAll(".cabinet-details__photo");
+/*Измененный скрипт перелистывания слайдов*/
 
-if (modalSearch) {
-  for (var i = 0; i < openButton.length; i++) openButton[i].addEventListener("click", function(event) {
-    event.preventDefault();
-    modalSearch.classList.add("slide-panel--on");
+$(document).ready(function() {
+  $(".carousel-slider").slick({
+    infinite: true,
+    slidesToShow: 11,
+    slidesToScroll: 1,
+    arrows: true,
   });
 
-  modalSearch.addEventListener("click", function() {
-    modalSearch.classList.remove("slide-panel--on");
-  });
+  var timer;
+    $(".slick-arrow").mouseleave(function() {
+      window.clearTimeout(timer)
+    }).mouseenter(function() {
 
-  modalSearch.addEventListener("click", function(event) {
-    event.stopPropagation();
-  });
-
-  closeButton.addEventListener("click", function() {
-    modalSearch.classList.remove("slide-panel--on");
-  });
-
-  window.addEventListener("keydown", function(event) {
-    if (event.keyCode === 27) {
-      modalSearch.classList.remove("slide-panel--on");
-    }
-  });
-}
-
-var modalRules = document.querySelector(".overlay");
-var closeRules = document.querySelector(".popup-rules__close");
-var openRules = document.querySelector(".footer-rules__link--howto")
-
-if (modalRules) {
-  for (var i = 0; i < openRules.length; i++) openRules[i].addEventListener("click", function(event) {
-    event.preventDefault();
-    modalRules.classList.add("overlay--on");
-  });
-
-  modalRules.addEventListener("click", function() {
-    modalRules.classList.remove("overlay--on");
-  });
-
-  modalRules.addEventListener("click", function(event) {
-    event.stopPropagation();
-  });
-
-  closeRules.addEventListener("click", function() {
-    modalRules.classList.remove("overlay--on");
-  });
-
-  window.addEventListener("keydown", function(event) {
-    if (event.keyCode === 27) {
-      modalRules.classList.remove("overlay--on");
-    }
-  });
-}
+      var self = $(this).mouseleave();
+      timer = window.setTimeout(function() {
+        self.click().mouseenter()
+      }, 2)
+})});
