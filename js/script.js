bfe1c4839addfc49b235c533d27c901793ceb047
nokia6680 <artur.ballod@gmail.com>
2018-12-12 01:46:44 +0200

    #######

--- js/script.js
+++ js/script.js
@@ -32,9 +32,42 @@ $(document).ready(function(){
 $(document).ready(function() {
   $(".carousel-slider").slick({
     infinite: true,
-    slidesToShow: 11,
+    slidesToShow: 3,
     slidesToScroll: 1,
     arrows: true,
+
+    prevArrow: '<div class="prev-1"></div>',
+    nextArrow: '<div class="next-1"></div>',
+
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          arrows: true
+        }
+      },
+      {
+        breakpoint: 414,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          arrows: true
+        }
+      },
+      {
+        breakpoint: 360,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          arrows: true
+        }
+      }
+    ]
   });
 
   var timer;
@@ -55,9 +88,22 @@ $(document).ready(function(){
     arrows: true,
     vertical: true,
     verticalSwiping: true,
-    slidesToShow: 5,
+    slidesToShow: 3,
     prevArrow: '<div class="prev-2"></div>',
     nextArrow: '<div class="next-2"></div>',
+
+    responsive: [
+      {
+        breakpoint: 360,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          arrows: false,
+          dots: false
+        }
+      }
+    ]
   });
 });
 
@@ -68,8 +114,21 @@ $(document).ready(function(){
     arrows: true,
     vertical: true,
     verticalSwiping: true,
-    slidesToShow: 5,
+    slidesToShow: 3,
     prevArrow: '<div class="prev-3"></div>',
     nextArrow: '<div class="next-3"></div>',
+
+    responsive: [
+      {
+        breakpoint: 360,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          arrows: false,
+          dots: false
+        }
+      }
+    ]
   });
 });
