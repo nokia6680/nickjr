06bdec0c38e5d699f82d7d76e16d7fa2883c3f00
nokia6680 <artur.ballod@gmail.com>
2018-12-18 03:47:29 +0200

    #768

--- js/script.js
+++ js/script.js
@@ -41,6 +41,15 @@ $(document).ready(function() {
 
     responsive: [
       {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 6.5,
+          slidesToScroll: 1,
+          infinite: true,
+          arrows: true
+        }
+      },
+      {
         breakpoint: 768,
         settings: {
           slidesToShow: 5,
@@ -52,7 +61,7 @@ $(document).ready(function() {
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 4,
           slidesToScroll: 1,
           infinite: true,
           arrows: true
@@ -94,7 +103,19 @@ $(document).ready(function(){
 
     responsive: [
       {
-        breakpoint: 360,
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          arrows: false,
+          dots: false,
+          vertical: false,
+          verticalSwiping: false
+        }
+      },
+      {
+        breakpoint: 768,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
@@ -120,7 +141,19 @@ $(document).ready(function(){
 
     responsive: [
       {
-        breakpoint: 360,
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          arrows: false,
+          dots: false,
+          vertical: false,
+          verticalSwiping: false
+        }
+      },
+      {
+        breakpoint: 768,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
@@ -146,7 +179,19 @@ $(document).ready(function(){
 
     responsive: [
       {
-        breakpoint: 360,
+        breakpoint: 1024,
+        settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: false,
+        dots: false,
+        vertical: false,
+        verticalSwiping: false
+      }
+    },
+    {
+      breakpoint: 768,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
@@ -172,8 +217,20 @@ $(document).ready(function(){
     nextArrow: '<div class="next-4"></div>',
 
     responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          arrows: false,
+          dots: false,
+          vertical: false,
+          verticalSwiping: false
+        }
+      },
       {
-        breakpoint: 360,
+        breakpoint: 768,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
@@ -199,8 +256,20 @@ $(document).ready(function(){
     nextArrow: '<div class="next-5"></div>',
 
     responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          arrows: false,
+          dots: false,
+          vertical: false,
+          verticalSwiping: false
+        }
+      },
       {
-        breakpoint: 360,
+        breakpoint: 768,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
