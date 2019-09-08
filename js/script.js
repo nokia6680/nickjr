72d67ff8750e435910874e7f813a9f40b3772a83
Artur Ballod <artur.ballod@gmail.com>
2019-09-08 13:58:59 +0200

    игр

--- js/script.js
+++ js/script.js
@@ -420,3 +420,65 @@ $(document).ready(function(){
     ]
   });
 });
+
+// Select
+$('.video-select').each(function() {
+  // Variables
+  var $this = $(this),
+    selectOption = $this.find('option'),
+    selectOptionLength = selectOption.length,
+    selectedOption = selectOption.filter(':selected'),
+    dur = 500;
+
+  $this.hide();
+  // Wrap all in select box
+  $this.wrap('<div class="video-select"></div>');
+  // Style box
+  $('<div>', {
+    class: 'video-select__gap',
+  }).insertAfter($this);
+
+  var selectGap = $this.next('.video-select__gap'),
+    caret = selectGap.find('.caret');
+  // Add ul list
+  $('<ul>', {
+    class: 'video-select__list'
+  }).insertAfter(selectGap);
+
+  var selectList = selectGap.next('.video-select__list');
+  // Add li - option items
+  for (var i = 0; i < selectOptionLength; i++) {
+    $('<li>', {
+        class: 'video-select__item',
+        html: $('<span>', {
+          text: selectOption.eq(i).text()
+        })
+      })
+      .attr('data-value', selectOption.eq(i).val())
+      .appendTo(selectList);
+  }
+  // Find all items
+  var selectItem = selectList.find('li');
+
+  selectList.slideUp(0);
+  selectGap.on('click', function() {
+    if (!$(this).hasClass('on')) {
+      $(this).addClass('on');
+      selectList.slideDown(dur);
+
+      selectItem.on('click', function() {
+        var chooseItem = $(this).data('value');
+
+        $('select').val(chooseItem).attr('selected', 'selected');
+        selectGap.text($(this).find('span').text());
+
+        selectList.slideUp(dur);
+        selectGap.removeClass('on');
+      });
+
+    } else {
+      $(this).removeClass('on');
+      selectList.slideUp(dur);
+    }
+  });
+});
