2053cc78ff8aa55e618a0009d11010fd8bca195f
nokia6680 <zenitfan@yandex.ru>
2019-10-14 00:53:24 +0200

    №№№№

--- js/script.js
+++ js/script.js
@@ -422,7 +422,7 @@ $(document).ready(function(){
 });
 
 // Select
-$('.video-select').each(function() {
+$('.select--first').each(function() {
   // Variables
   var $this = $(this),
     selectOption = $this.find('option'),
@@ -432,24 +432,88 @@ $('.video-select').each(function() {
 
   $this.hide();
   // Wrap all in select box
-  $this.wrap('<div class="video-select"></div>');
+  $this.wrap('<div class="select"></div>');
   // Style box
   $('<div>', {
-    class: 'video-select__gap',
+    class: 'select__gap',
+    text: 'Сезон'
   }).insertAfter($this);
 
-  var selectGap = $this.next('.video-select__gap'),
+  var selectGap = $this.next('.select__gap'),
     caret = selectGap.find('.caret');
   // Add ul list
   $('<ul>', {
-    class: 'video-select__list'
+    class: 'select__list'
   }).insertAfter(selectGap);
 
-  var selectList = selectGap.next('.video-select__list');
+  var selectList = selectGap.next('.select__list');
   // Add li - option items
   for (var i = 0; i < selectOptionLength; i++) {
     $('<li>', {
-        class: 'video-select__item',
+        class: 'select__item',
+        html: $('<span>', {
+          text: selectOption.eq(i).text()
+        })
+      })
+      .attr('data-value', selectOption.eq(i).val())
+      .appendTo(selectList);
+  }
+  // Find all items
+  var selectItem = selectList.find('li');
+
+  selectList.slideUp(0);
+  selectGap.on('click', function() {
+    if (!$(this).hasClass('on')) {
+      $(this).addClass('on');
+      selectList.slideDown(dur);
+
+      selectItem.on('click', function() {
+        var chooseItem = $(this).data('value');
+
+        $('select').val(chooseItem).attr('selected', 'selected');
+        selectGap.text($(this).find('span').text());
+
+        selectList.slideUp(dur);
+        selectGap.removeClass('on');
+      });
+
+    } else {
+      $(this).removeClass('on');
+      selectList.slideUp(dur);
+    }
+  });
+});
+
+// Select
+$('.select--second').each(function() {
+  // Variables
+  var $this = $(this),
+    selectOption = $this.find('option'),
+    selectOptionLength = selectOption.length,
+    selectedOption = selectOption.filter(':selected'),
+    dur = 500;
+
+  $this.hide();
+  // Wrap all in select box
+  $this.wrap('<div class="select"></div>');
+  // Style box
+  $('<div>', {
+    class: 'select__gap',
+    text: 'Серия'
+  }).insertAfter($this);
+
+  var selectGap = $this.next('.select__gap'),
+    caret = selectGap.find('.caret');
+  // Add ul list
+  $('<ul>', {
+    class: 'select__list'
+  }).insertAfter(selectGap);
+
+  var selectList = selectGap.next('.select__list');
+  // Add li - option items
+  for (var i = 0; i < selectOptionLength; i++) {
+    $('<li>', {
+        class: 'select__item',
         html: $('<span>', {
           text: selectOption.eq(i).text()
         })
