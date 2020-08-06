27363b1d9e0bee60c078346a0859b2893b99f543
Artur Ballod <artur.ballod@gmail.com>
2020-08-07 00:40:29 +0200

    12323123

--- js/script.js
+++ js/script.js
@@ -152,147 +152,10 @@ $(document).ready(function() {
     }
   });
 });
-/*Первый слайдер на странице INDEX*/
-$(document).ready(function(){
-  $('.sidebar-slider--first').slick({
-    infinite: true,
-    dots: false,
-    arrows: true,
-    vertical: true,
-    verticalSwiping: true,
-    slidesToShow: 5,
-    prevArrow: '<div class="prev-2"></div>',
-    nextArrow: '<div class="next-2"></div>',
 
-    responsive: [
-      {
-        breakpoint: 1600,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          arrows: true,
-          dots: false,
-          vertical: true,
-          verticalSwiping: true
-        }
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 1,
-          infinite: true,
-          arrows: false,
-          dots: false,
-          vertical: false,
-          verticalSwiping: false
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          arrows: false,
-          dots: false
-        }
-      }
-    ]
-  });
-});
-/*Второй слайдер на странице INDEX*/
-$(document).ready(function(){
-  $('.sidebar-slider--second').slick({
-    infinite: true,
-    dots: false,
-    arrows: true,
-    vertical: true,
-    verticalSwiping: true,
-    slidesToShow: 5,
-    prevArrow: '<div class="prev-3"></div>',
-    nextArrow: '<div class="next-3"></div>',
-
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 1,
-          infinite: true,
-          arrows: false,
-          dots: false,
-          vertical: false,
-          verticalSwiping: false
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          arrows: false,
-          dots: false
-        }
-      }
-    ]
-  });
-});
-/*Слайдер на странице SHOW*/
-$(document).ready(function(){
-  $('.sidebar-slider--show').slick({
-    infinite: true,
-    dots: false,
-    arrows: true,
-    vertical: true,
-    verticalSwiping: true,
-    slidesToShow: 5,
-    prevArrow: '<div class="prev-3"></div>',
-    nextArrow: '<div class="next-3"></div>',
-
-    responsive: [
-        {
-          breakpoint: 1600,
-          settings: {
-          slidesToShow: 5,
-          slidesToScroll: 1,
-          infinite: true,
-          arrows: false,
-          dots: false,
-          vertical: false,
-          verticalSwiping: false
-        }
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        infinite: true,
-        arrows: false,
-        dots: false,
-        vertical: false,
-        verticalSwiping: false
-      }
-    },
-    {
-      breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          arrows: false,
-          dots: false
-        }
-      }
-    ]
-  });
-});
 /*Первый слайдер на странице Game*/
 $(document).ready(function(){
-  $('.game-slider--similar').slick({
+  $('.similar__slider').slick({
     infinite: true,
     dots: false,
     arrows: true,
@@ -331,6 +194,7 @@ $(document).ready(function(){
         breakpoint: 768,
         settings: {
           slidesToShow: 2,
+          slidesPerRow: 2,
           slidesToScroll: 1,
           infinite: true,
           arrows: false,
